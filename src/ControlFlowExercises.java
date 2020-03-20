decf22ff68132a7ad844260f2046f3e9f7b292fb
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-20 17:02:25 -0500

    struggling

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,20 +1,70 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main (String[] args){
-        //Exercise 1
+    //Exercise 1
+        //While
+            //int i = 5;
+            //while (i <= 15) {
+                //System.out.println("i = " + i);
+                //++i;
+            //};
+
+        //Do While
+            //int x = 0;
+            //do {
+                //System.out.println(x);
+                //x+=2;
+            //}while(x <= 100);
+
+            //int y = 100;
+            //do {
+                //System.out.println(y);
+                //y-=5;
+            //}while(y >= -10);
+
+            //long z = 2;
+            //do {
+                //System.out.println(z);
+                //z *= z;
+            //} while (z < 1000000);
+
+        //For
+//            for (int y = 100; y >= -10; y-=5){
+//                System.out.println(y);
+//            };
+
+//            for (long z = 2; z < 1000000; z*=z){
+//                System.out.println(z);
+//            };
+
+    //Exercise 2
+        //for (int x = 1; x <= 100; ++x){
+            //if (x % 15 == 0){
+               // System.out.println("Fizzbuzz");
+            //}
+            //if (x % 5 == 0){
+                //System.out.println("Buzz");
+            //}
+            //if (x % 3 == 0){
+                //System.out.println("Fizz");
+            //}
+            //else{
+                //System.out.println(x);
+            //};
+        //};
+
+    //Exercise 3
+//        Scanner myInput = new Scanner(System.in);
+//        System.out.println("Please enter an integer: ");
+//        int userInt = myInput.nextInt();
+//        System.out.println(userInt);
 
 
 
 
+    //Exercise 4
 
-
-
-
-
-
-
-
-
-
-        //Exercise 2
     }
 }
