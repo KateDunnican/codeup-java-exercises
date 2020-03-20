ae43a26baa3d43dad01b64cf384a89886d4f2f17
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-20 12:30:37 -0500

    java day 2

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f", pi); //.2 for 2 decimal places
+
+        Scanner myInput = new Scanner(System.in);
+
+//        System.out.println("Please enter an integer: ");
+//        int userInt = myInput.nextInt(); //asks for your input in the console
+//        System.out.println(userInt);
+
+//        System.out.println("Please enter 3 words");
+//        String word1 = myInput.next();
+//        String word2 = myInput.next();
+//        String word3 = myInput.next();
+//        System.out.println(word1);
+//        System.out.println(word2);
+//        System.out.println(word3);
+
+//        System.out.println("Please enter a sentence: ");
+//        String sentence = myInput.next(); //only gets first word, not whole line
+//        String sentence = myInput.nextLine(); //gets the whole sentence
+//        System.out.println(sentence);
+
+        System.out.println("Please enter a length: ");
+        int length = Integer.parseInt(myInput.nextLine());
+        System.out.println("Please enter a width: ");
+        int width = Integer.parseInt(myInput.nextLine());
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,53 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //these are all statements, can be with or without {}
+        {
+            System.out.println("Hello, World!");
+            System.out.print("Hello ");
+            System.out.print("World\n");
+        }
+
+        //Types:
+            //Primitive
+                //byte - Very short integers from -128 to 127
+                //short - Short integers from -32,768 to 32,767
+                //int - Integers from -2,147,483,648 to 2,147,483,647
+                //long - Integers pos or neg past the trillions
+                //float - Single-precision, decimal numbers
+                    //from -3.4E38 to 3.4E38 with up to 7 significant digits
+                //double - Double-precision, floating-point numbers
+                    //from -1.7E308 to 1.7E308 with up to 16 significant digits
+                //char - A single Unicode character that’s stored in two bytes
+                //boolean - A true or false value
+            //Reference
+
+        //Variables: named likeThis
+            // type varName = the variable;
+
+        //Constant: named LIKE_THIS
+            //final int/string/whatever
+
+        //Variable
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+
+        //String
+        String myString = "I made a string, yay!";
+        System.out.println(myString);
+
+        char myChar = 'D';
+        System.out.println(myChar);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+        float myOtherNumber = 3.14f;
+        System.out.println(myOtherNumber);
+
+        //sout + tab
+
+
+
+
+    }
+}
