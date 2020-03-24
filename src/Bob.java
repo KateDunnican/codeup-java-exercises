549b4fef69d29732bbce6d014d5a56ede5387ae2
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-24 10:22:23 -0500

    more exercises

---
+++ src/Bob.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main (String[] args){
+////      Exercise 2/2 (Strings)
+//        Scanner sc = new Scanner (System.in);
+//        System.out.println("Say something to Bob:");
+//
+//        String userInput = sc.nextLine();
+//
+//        if (userInput.endsWith("?")){
+//            System.out.println("Sure.");
+//        }
+//        if (userInput.endsWith("!")){
+//            System.out.println("Whoa, chill out!");
+//        }
+//        if (userInput.equals(" ")){
+//            System.out.println("Fine.  Be that way!");
+//        }
+//        else{
+//            System.out.println("Whatever.");
+//        }
+
+        String x = "Hello";
+        int y = 5;
+        System.out.println(x + y);
+
+    }
+}
---
+++ src/MethodsExercises.java
@@ -0,0 +1,8 @@
+public class MethodsExercises {
+
+//    Exercise 1
+//    Exercise 2
+//    Exercise 3
+//    Exercise 4
+
+}
