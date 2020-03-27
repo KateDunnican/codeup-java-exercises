2b6a78163f2eb9d244c5d10be18439d1e073a5a2
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-27 09:57:55 -0500

    finished exercise 1

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,25 +1,32 @@
+import java.sql.Array;
 import java.util.Arrays;
 
 public class ArraysExercises {
 
-
-
-//    public static void addPerson (Person[], Person){
-//        return Arrays.copyOf(people, people.length + 1);
-//    }
+    public static Person[] addPerson (Person[] personArray, Person newPerson){
+        Person[] newArray = Arrays.copyOf(personArray, personArray.length + 1);
+        newArray[3] = new Person(newPerson.getName());
+        return newArray;
+    }
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
+
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
 
         Person[] people = new Person[3];
             people[0]= new Person("Casey");
             people[1]= new Person("Vivian");
             people[2]= new Person("Trant");
 
-        int x = Person people.length;
+            int x = people.length;
 
-            for (Person individual : people){
+//            for (Person individual : people){
+//                System.out.println(individual.getName());
+//            }
+
+//            addPerson(people, new Person("Jimbo"));
+            for (Person individual : addPerson(people, new Person("Jimbo"))){
                 System.out.println(individual.getName());
             }
     }
