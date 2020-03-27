f2cf7b05f6d816cab5409a3281194aa065034151
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-27 10:05:13 -0500

    added comments

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+    // Method to add a Person object to the people array by making a new array
     public static Person[] addPerson (Person[] personArray, Person newPerson){
         Person[] newArray = Arrays.copyOf(personArray, personArray.length + 1);
         newArray[3] = new Person(newPerson.getName());
@@ -10,24 +11,26 @@ public class ArraysExercises {
     }
 
     public static void main(String[] args) {
+        // Iterating through an array of numbers
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
 
-//        int[] numbers = {1, 2, 3, 4, 5};
-//        System.out.println(Arrays.toString(numbers));
-
+        // an array of Person objects named "people"
         Person[] people = new Person[3];
             people[0]= new Person("Casey");
             people[1]= new Person("Vivian");
             people[2]= new Person("Trant");
 
-            int x = people.length;
 
-//            for (Person individual : people){
-//                System.out.println(individual.getName());
-//            }
+            // Iterating through people
+            for (Person individual : people){
+                System.out.println(individual.getName());
+            }
 
-//            addPerson(people, new Person("Jimbo"));
+            // Iterating through people + Jimbo (newArray from addPerson)
             for (Person individual : addPerson(people, new Person("Jimbo"))){
                 System.out.println(individual.getName());
             }
     }
+
 }
