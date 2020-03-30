6ecabf8430cea3ebc6213f568a30ea8a13168e69
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-30 10:27:12 -0500

    more changes

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -2,18 +2,16 @@ import java.util.Random;
 
 public class ServerNameGenerator {
 
+    public static String[] adjectives = {"fluffy", "fuzzy", "spotted", "chonky", "needy", "lizardly", "sunny", "pointy", "bulbous", "enormous"};
+    public static String[] nouns = {"dog", "cat", "string", "bird", "cactus", "pan", "cement", "laptop", "water", "dirt"};
+
     public static String combinationMaker(String[] x, String[] y){
-        String randomAdj = x[new Random().nextInt(10)];
-        String randomNoun = y[new Random().nextInt(10)];
+        String randomAdj = x[new Random().nextInt(x.length)];
+        String randomNoun = y[new Random().nextInt(y.length)];
         return randomAdj + "-" + randomNoun;
     }
 
     public static void main(String[] args) {
-        //First way of initiating an array
-        String[] adjectives = {"fluffy", "fuzzy", "spotted", "chonky", "needy", "lizardly", "sunny", "pointy", "bulbous", "enormous"};
-
-        String[] nouns = {"dog", "cat", "string", "bird", "cactus", "pan", "cement", "laptop", "water", "dirt"};
-
         System.out.println(combinationMaker(adjectives, nouns));
     }
 }
