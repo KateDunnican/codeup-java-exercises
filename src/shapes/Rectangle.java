27d71ae79a6be099489786c31643e2cd5730a1e2
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-30 16:32:49 -0500

    finished polymorph and inheritance exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -9,11 +9,11 @@ public class Rectangle {
         this.width = w;
     }
 
-    public int getArea(){
-        return (length * width);
-    }
+        public int getPerimeter(){
+            return ((2*length)+(2*width));
+        }
 
-    public int getPerimeter(){
-        return ((2*length)+(2*width));
-    }
+        public int getArea(){
+            return (length * width);
+        }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,13 +1,24 @@
 package shapes;
 
-import org.w3c.dom.css.Rect;
+import java.lang.invoke.SwitchPoint;
 
 public class Square extends Rectangle {
 
-    public Square (int length, int width, int side){ //constructor
-      super(length, width);
-      this.length = side;
-      this.width = side;
+    protected int side;
+
+    public Square (int side){
+        //constructor calls Rectangle constructor and replaces "l" and "w" with "side"
+        super(side, side);
+        this.side = side;
     }
 
+        @Override
+        public int getPerimeter(){
+            return (4 * side);
+        }
+
+        @Override
+        public int getArea() {
+            return (side * side);
+        }
 }
