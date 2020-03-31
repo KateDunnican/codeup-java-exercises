7971e3cae5860910df73c326ba3f89d8f63f398d
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-31 11:02:57 -0500

    working on interface and abstract exercises

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,24 +1,22 @@
 package shapes;
 
-import java.lang.invoke.SwitchPoint;
-
-public class Square extends Rectangle {
-
-    protected int side;
-
-    public Square (int side){
-        //constructor calls Rectangle constructor and replaces "l" and "w" with "side"
-        super(side, side);
-        this.side = side;
-    }
-
-        @Override
-        public int getPerimeter(){
-            return (4 * side);
-        }
-
-        @Override
-        public int getArea() {
-            return (side * side);
-        }
-}
+//public class Square extends Rectangle {
+//
+//    protected int side;
+//
+//    public Square (int side){
+//        //constructor calls Rectangle constructor and replaces "l" and "w" with "side"
+//        super(side, side);
+//        this.side = side;
+//    }
+//
+//        @Override
+//        public int getPerimeter(){
+//            return (4 * side);
+//        }
+//
+//        @Override
+//        public int getArea() {
+//            return (side * side);
+//        }
+//}
