8b00e1178ddbdda9a10910953cca0b8b922cea1d
Kate McKinney <kate.mckinney210@gmail.com>
2020-04-01 11:34:06 -0500

    doing exercises for collections

---
+++ src/grades/Student.java
@@ -0,0 +1,37 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String studentName;
+    private ArrayList<Integer> studentGrades;
+
+    //constructor
+    public Student(String x){
+        this.studentName = x;
+        this.studentGrades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName(){
+        return studentName;
+    }
+
+    // adds a grade
+    public void addGrade(int grade){
+        studentGrades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double allTheGrades = 0;
+        for (double grade: studentGrades){
+            allTheGrades += grade;
+        }
+        return (allTheGrades / studentGrades.size());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
